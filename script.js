dafdc185d23f158c478f911eaa7fc96bbbf650a7
bonemuffin <101133295+BoneMuffin@users.noreply.github.com>
2023-01-16 19:10:41 -0800

    cleaner code

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
 // UI 
-const addBookForm = document.getElementById('addBookForm');
 const addBookBtn = document.getElementById('addBookBtn');
 const addBookModal = document.getElementById('addBookModal');
-const accountModal = document.getElementById('accountModal');
-const overlay = document.getElementById('overlay');
+const addBookForm = document.getElementById('addBookForm');
+
 const booksGrid = document.getElementById('booksGrid');
+const overlay = document.getElementById('overlay');
 const loadingRing = document.getElementById('loadingRing');
 
 // empty array for library
@@ -28,16 +28,22 @@ constructor(
 
 // add user input-new object into array
 function addBookToLibrary(title, author, pages, readStatus) {
+    new Book(title, author, pages, readStatus)
 };
 
 
 // function that loops through array and displays books on page
 
+
 // 'new book' button, with a form / event.preventDefault documentation //
+
 
 // remove button on each book card, associate dom elements w/ book objects //
 /* giving them a data-attribute that corresponds to the index of the library array. */
 
+
 // add button to change read status 
 /* create the function that toggles a book’s read status 
 on your Book prototype instance. */
+
+//function to display bookshelf
