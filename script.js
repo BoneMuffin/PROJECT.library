b4f74133810e7282f74d0b29a39b8c140fbca495
bonemuffin <101133295+BoneMuffin@users.noreply.github.com>
2023-01-19 12:39:43 -0800

    re factor with clean comment

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// UI 
+// User interface
 const addBookBtn = document.getElementById('addBookBtn');
 const addBookModal = document.getElementById('addBookModal');
 const addBookForm = document.getElementById('addBookForm');
@@ -7,6 +7,7 @@ const booksGrid = document.getElementById('booksGrid');
 const overlay = document.getElementById('overlay');
 const loadingRing = document.getElementById('loadingRing');
 
+const myLibrary = [];
 
 // object constructor
 class Book {
@@ -23,128 +24,29 @@ constructor(
     }
 };
 
-// empty array for library + store new object into array
-class Library {
-    constructor() {
-        this.books = []
-    }
+function addBookToLibrary() {
 
-    addBookToLibrary(newBook) {
-        if (!this.isInLibrary(newBook)) {
-            this.books.push(newBook)
-        }
-    }
-
-    removeBook(title) {
-        this.books = this.books.filter((book) => book.title !== title)
-      }
-    
-    getBook(title) {
-        return this.books.find((book) => book.title === title)
-      }
-    
-    isInLibrary(newBook) {
-        return this.books.some((book) => book.title === newBook.title)
-      }
-};
-
-const library = new Library();
-
-const updateBooksGrid = () => {
-    resetBooksGrid()
-    for (let book of library.books) {
-      createBookCard(book)
-    }
-};
-  
-const resetBooksGrid = () => {
-    booksGrid.innerHTML = ''
-};
-
-// loop through array and display book cards
-const displayBookshelf = (book) => {
-        const bookCard = document.createElement('div')
-        const title = document.createElement('p')
-        const author = document.createElement('p')
-        const pages = document.createElement('p')
-        const bookOptions = document.createElement('div')
-        const readBtn = document.createElement('button')
-        const removeBtn = document.createElement('button')
-    
-    
-        bookCard.classList.add('bookCard')
-        bookOptions.classList.add('bookOptions')
-        readBtn.classList.add('btn')
-        removeBtn.classList.add('btn')
-        readBtn.onclick = toggleRead
-        removeBtn.onclick = removeBook
-
-        title.textContent = `"${book.title}"`
-        author.textContent = book.author
-        pages.textContent = `${book.pages} pages`
-        removeBtn.textContent = 'Remove'
-        
-        if (book.readStatus) {
-            readBtn.textContent = 'Read'
-            readBtn.classList.add('readBtn')
-          } else {
-            readBtn.textContent = 'Not read'
-            readBtn.classList.add('removeBtn')
-          }
-        
-          bookCard.appendChild(title)
-          bookCard.appendChild(author)
-          bookCard.appendChild(pages)
-          bookOptions.appendChild(readBtn)
-          bookOptions.appendChild(removeBtn)
-          bookCard.appendChild(bookOptions)
-          booksGrid.appendChild(bookCard)
-};
-
-// to take user input
-const getUserInput = () => {
-  const title = document.getElementById('title').value
-  const author = document.getElementById('author').value
-  const pages = document.getElementById('pages').value
-  const readStatus = document.getElementById('isRead').checked
-  return new Book(title, author, pages, readStatus)
 }
 
+/* add a function to the script (not the constructor) that can take user’s input 
+and store the new book objects into an array. */
 
-// 'new book' button, with a form / event.preventDefault documentation //
+/* a function that loops through the array and displays each book on the page. 
+You can display them in some sort of table, or each on their own “card”.*/
 
 
+/* “NEW BOOK” button that brings up a form allowing users to input the details for the new book:
+ author, title, number of pages, whether it’s been read */
 
-// remove button on each book card, associate dom elements w/ book objects //
-/* giving them a data-attribute that corresponds to the index of the library array. */
-const removeBook = (e) => {
-  const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll('"','')
 
-  if (auth.currentUser) {
-    removeBookDB(title)
-  } else {
-    library.removeBook(title)
-    saveLocal()
-    updateBooksGrid()
-  }
-}
+/* Add a button on each book’s display to remove the book from the library.
+  You will need to associate your DOM elements with the actual book objects in some way. 
+  One easy solution is giving them a data-attribute that corresponds to the index of the 
+  library array.
+ */
 
-// add button to change read status 
-/* create the function that toggles a book’s read status 
-on your Book prototype instance. */
 
-const toggleRead = (e) => {
-  const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
-    '"',
-    ''
-  )
-  const book = library.getBook(title)
-
-  if (auth.currentUser) {
-    toggleBookIsReadDB(book)
-  } else {
-    book.isRead = !book.isRead
-    saveLocal()
-    updateBooksGrid()
-  }
-}
+/* Add a button on each book’s display to change its read status.
+  To facilitate this you will want to create the function that toggles a book’s read status
+  on your Book prototype instance.
+*/
