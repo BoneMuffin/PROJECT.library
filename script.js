a09ac890ae8a14e0c26c1f75edd064816d08fd3f
bonemuffin <101133295+BoneMuffin@users.noreply.github.com>
2023-01-23 11:16:38 -0800

    Update
    completed five main problems with pseudocode included

--- script.js
+++ script.js
@@ -7,8 +7,6 @@ const booksGrid = document.getElementById('booksGrid');
 const overlay = document.getElementById('overlay');
 const loadingRing = document.getElementById('loadingRing');
 
-const myLibrary = [];
-
 // object constructor
 class Book {
 constructor(
@@ -24,29 +22,185 @@ constructor(
     }
 };
 
-function addBookToLibrary() {
+class Library {
+  constructor() {
+    this.books = []
+  }
 
+  addBook(newBook) {
+    if (!this.isInLibrary(newBook)) {
+      this.books.push(newBook)
+    }
+  }
+
+  removeBook(title) {
+    this.books = this.books.filter((book) => book.title !== title)
+  }
+
+  getBook(title) {
+    return this.books.find((book) => book.title === title)
+  }
+
+  isInLibrary(newBook) {
+    return this.books.some((book) => book.title === newBook.title)
+  }
+};
+
+const library = new Library()
+
+// base books
+const year1984 = new Book("year1984", "G. Orwell", 333, true);
+const harryPotter = new Book("Harry Potter", "J. Rowling", 400, true);
+
+
+const openAddBookModal = () => {
+  addBookForm.reset()
+  addBookModal.classList.add('active')
+  overlay.classList.add('active')
+};
+
+const closeAddBookModal = () => {
+  addBookModal.classList.remove('active')
+  overlay.classList.remove('active')
+  errorMsg.classList.remove('active')
+  errorMsg.textContent = ''
+};
+
+const handleKeyboardInput = (e) => {
+  if (e.key === 'Escape') closeAllModals()
 }
 
-/* add a function to the script (not the constructor) that can take user’s input 
-and store the new book objects into an array. */
 
-/* a function that loops through the array and displays each book on the page. 
-You can display them in some sort of table, or each on their own “card”.*/
+const updateBooksGrid = () => {
+  resetBooksGrid()
+  for (let book of library.books) {
+    createBookCard(book)
+  }
+};
 
+const resetBooksGrid = () => {
+  booksGrid.innerHTML = ''
+};
 
-/* “NEW BOOK” button that brings up a form allowing users to input the details for the new book:
- author, title, number of pages, whether it’s been read */
+// a function that loops through the array and displays each book on the page.
+const renderBooks = (book) => {
+  const bookCard = document.createElement('div')
+  const title = document.createElement('p')
+  const author = document.createElement('p')
+  const pages = document.createElement('p')
+  const buttonGroup = document.createElement('div')
+  const readBtn = document.createElement('button')
+  const removeBtn = document.createElement('button')
 
+  bookCard.classList.add('book-card')
+  buttonGroup.classList.add('bookOptions')
+  readBtn.classList.add('btn')
+  removeBtn.classList.add('btn')
+  readBtn.onclick = toggleRead
+  removeBtn.onclick = removeBook
 
-/* Add a button on each book’s display to remove the book from the library.
-  You will need to associate your DOM elements with the actual book objects in some way. 
-  One easy solution is giving them a data-attribute that corresponds to the index of the 
-  library array.
- */
+  title.textContent = `"${book.title}"`
+  author.textContent = book.author
+  pages.textContent = `${book.pages} pages`
+  removeBtn.textContent = 'Remove'
 
+  if (book.isRead) {
+    readBtn.textContent = 'Read'
+    readBtn.classList.add('readBtn')
+  } else {
+    readBtn.textContent = 'Not read'
+    readBtn.classList.add('removeBtn')
+  }
 
-/* Add a button on each book’s display to change its read status.
-  To facilitate this you will want to create the function that toggles a book’s read status
-  on your Book prototype instance.
+  bookCard.appendChild(title)
+  bookCard.appendChild(author)
+  bookCard.appendChild(pages)
+  buttonGroup.appendChild(readBtn)
+  buttonGroup.appendChild(removeBtn)
+  bookCard.appendChild(buttonGroup)
+  booksGrid.appendChild(bookCard)
+};
+
+/* function to the script (not the constructor) that can take user’s input 
+and store the new book objects into an array
 */
+const addBookToLibrary = () => {
+  const title = document.getElementById('title').value
+  const author = document.getElementById('author').value
+  const pages = document.getElementById('pages').value
+  const readStatus = document.getElementById('readStatus').checked
+  return new Book(title, author, pages, readStatus)
+};
+
+//form allowing users to input the details for new book
+const addBook = (e) => {
+  e.preventDefault()
+  const newBook = addBookToLibrary()
+
+  if (library.isInLibrary(newBook)) {
+    errorMsg.textContent = 'This book already exists in your library'
+    errorMsg.classList.add('active')
+    return
+  }
+
+  if (auth.currentUser) {
+    addBookDB(newBook)
+  } else {
+    library.addBook(newBook)
+    saveLocal()
+    updateBooksGrid()
+  }
+
+  closeAddBookModal()
+}
+ // remove the book from the library.
+const removeBook = (e) => {
+  const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
+    '"',
+    ''
+  )
+
+  if (auth.currentUser) {
+    removeBookDB(title)
+  } else {
+    library.removeBook(title)
+    saveLocal()
+    updateBooksGrid()
+  }
+}
+
+// button on each book’s display to change its read status. 
+const toggleRead = (e) => {
+  const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
+    '"',
+    ''
+  )
+  const book = library.getBook(title)
+
+  if (auth.currentUser) {
+    toggleBookIsReadDB(book)
+  } else {
+    book.isRead = !book.isRead
+    saveLocal()
+    updateBooksGrid()
+  }
+}
+
+addBookBtn.onclick = openAddBookModal
+overlay.onclick = closeAddBookModal
+addBookForm.onsubmit = addBook
+window.onkeydown = handleKeyboardInput
+
+// Local Storage
+const saveLocal = () => {
+  localStorage.setItem('library', JSON.stringify(library.books))
+};
+
+const restoreLocal = () => {
+  const books = JSON.parse(localStorage.getItem('library'))
+  if (books) {
+    library.books = books.map((book) => JSONToBook(book))
+  } else {
+    library.books = []
+  }
+};
