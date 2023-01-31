1a51cff275b40aa0a1f039713376aad58c6cafd0
bonemuffin <101133295+BoneMuffin@users.noreply.github.com>
2023-01-31 13:53:14 -0800

    cleaner constructor

--- script.js
+++ script.js
@@ -10,42 +10,66 @@ let myLibrary = [];
 
 // constructor
 class Book {
-  constructor(
-    title = 'Unknown',
-    author = 'Unknown',
-    pages = 0,
-    readStatus = false
-    ) {
+  constructor(title, author, pages, isRead) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.readStatus = readStatus;
-    }
+    this.isRead = isRead;
+  }
 };
 
-// hardcode books 
+// hardcoded books 
 const aliceBook = new Book('Alice in Wonderland', 'Lewis Carroll', 52, true); 
 const lotrBook = new Book('Lord of the Rings', 'J. R. R. Tolkien', 1178, false);
 
-/* function to the script (not the constructor) that can take user’s input 
-and store the new book objects into an array */
-// Add new book to myLibrary array 
-
-function addBookToLibrary() {
-  let book = new Book(title, author, pages, readStatus);
+// take user’s input and store new book objects into array 
+function addBookToLibrary(title, author, pages, isRead) {
+  let book = new Book(title, author, pages, isRead);
   myLibrary.push(book);
+  renderBooks();
 
 	localStorage.setItem('library', JSON.stringify(myLibrary));
-
 };
-/* function that loops through the array and displays each book on the page.
- You can display them in some sort of table, or each on their own “card”.  */
 
+// function that loops through the array and displays each book 
 const renderBooks = () => {
+  const books = document.querySelector('.bookCard');
 
 };
+
 /* “NEW BOOK” button that brings up a form allowing users to input
- the details for the new book. event.preventDefault();*/
+ the details for the new book. event.preventDefault();
+*/
+const openAddBookModal = () => {
+  addBookForm.reset()
+  addBookModal.classList.add('active')
+  overlay.classList.add('active')
+}; 
+
+const closeAddBookModal = () => {
+  addBookModal.classList.remove('active')
+  overlay.classList.remove('active')
+  errorMsg.classList.remove('active')
+  errorMsg.textContent = ''
+};
+
+const closeModal = () => {
+  closeAddBookModal()
+};
+
+const handleKeyboardInput = (e) => {
+  if (e.key === 'Escape') closeModal()
+};
+
+// mouse event listeners 
+addBookBtn.onclick = openAddBookModal;
+
+const addBookButton = document.querySelector('.ddBookBtn')
+addBookButton.addEventListener('click', openAddBookModal)
+
+overlay.onclick = closeModal;
+addBookForm.onsubmit = addBook;
+window.onkeydown = handleKeyboardInput;
 
 /* 
 Add a button on each book’s display to remove the book from the library.
@@ -54,32 +78,12 @@ objects in some way. One easy solution is giving them a
 data-attribute that corresponds to the index of the library array.
 */
 
-const removeBook = () => {
 
-};
+
 /*
 Add a button on each book’s display to change its read status.
 To facilitate this you will want to create the function that
  toggles a book’s read status on your Book prototype instance.
 */
+const toggleStatus () = {};
 
-const toggleStatus = () => {
-
-};
-
-const openAddBookModal = () => {
-  addBookForm.reset()
-  addBookModal.classList.add('active')
-  overlay.classList.add('active')
-}
-
-const closeAddBookModal = () => {
-  addBookModal.classList.remove('active')
-  overlay.classList.remove('active')
-  errorMsg.classList.remove('active')
-  errorMsg.textContent = ''
-}
-
-const handleKeyboardInput = (e) => {
-  if (e.key === 'Escape') closeAddBookModal()
-}
