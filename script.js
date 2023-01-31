4ce70222b928c061b18082b917ad8cf898e155b3
bonemuffin <101133295+BoneMuffin@users.noreply.github.com>
2023-01-30 18:18:11 -0800

    add function that adds new book to array

--- script.js
+++ script.js
@@ -25,20 +25,25 @@ class Book {
 
 // hardcode books 
 const aliceBook = new Book('Alice in Wonderland', 'Lewis Carroll', 52, true); 
-const ringBook = new Book('Lord of the Rings', 'J. R. R. Tolkien', 1178, false);
+const lotrBook = new Book('Lord of the Rings', 'J. R. R. Tolkien', 1178, false);
 
 /* function to the script (not the constructor) that can take user’s input 
 and store the new book objects into an array */
 // Add new book to myLibrary array 
+
 function addBookToLibrary() {
-  myLibrary.push(object);
+  let book = new Book(title, author, pages, readStatus);
+  myLibrary.push(book);
+
+	localStorage.setItem('library', JSON.stringify(myLibrary));
+
 };
 /* function that loops through the array and displays each book on the page.
  You can display them in some sort of table, or each on their own “card”.  */
 
-const renderBooks {
+const renderBooks = () => {
 
-}
+};
 /* “NEW BOOK” button that brings up a form allowing users to input
  the details for the new book. event.preventDefault();*/
 
@@ -49,134 +54,32 @@ objects in some way. One easy solution is giving them a
 data-attribute that corresponds to the index of the library array.
 */
 
+const removeBook = () => {
+
+};
 /*
 Add a button on each book’s display to change its read status.
 To facilitate this you will want to create the function that
  toggles a book’s read status on your Book prototype instance.
 */
 
+const toggleStatus = () => {
 
+};
 
-
-
-
-
-
-
-
-/* modal events
 const openAddBookModal = () => {
   addBookForm.reset()
   addBookModal.classList.add('active')
   overlay.classList.add('active')
-};
+}
 
 const closeAddBookModal = () => {
   addBookModal.classList.remove('active')
   overlay.classList.remove('active')
   errorMsg.classList.remove('active')
   errorMsg.textContent = ''
-};
-
+}
 
 const handleKeyboardInput = (e) => {
   if (e.key === 'Escape') closeAddBookModal()
-};
-
-const updateBooksGrid = () => {
-  resetBooksGrid()
-  for (let book of library.books) {
-    createBookCard(book)
-  }
-};
-
-const resetBooksGrid = () => {
-  booksGrid.innerHTML = ''
-};
-
-// a function that loops through the array and displays each book on the page.
-const renderBooks = (book) => {
-  const bookCard = document.createElement('div')
-  const title = document.createElement('p')
-  const author = document.createElement('p')
-  const pages = document.createElement('p')
-  const buttonGroup = document.createElement('div')
-  const readBtn = document.createElement('button')
-  const removeBtn = document.createElement('button')
-
-};
-
-// function that can take user’s input and store the new book objects into an array
-const addBookToLibrary = () => {
-  const title = document.getElementById('title').value
-  const author = document.getElementById('author').value
-  const pages = document.getElementById('pages').value
-  const readStatus = document.getElementById('readStatus').checked
-  return new Book(title, author, pages, readStatus)
-};
-
-// form allowing users to input details for new book
-const addBook = (e) => {
-  e.preventDefault()
-  const newBook = addBookToLibrary()
-
-  if (library.isInLibrary(newBook)) {
-    errorMsg.textContent = 'This book already exists in your library'
-    errorMsg.classList.add('active')
-    return
-  }
-
-  if (auth.currentUser) {
-    addBookDB(newBook)
-  } else {
-    library.addBook(newBook)
-    saveLocal()
-    updateBooksGrid()
-  }
-
-  closeAddBookModal()
-};
-
- // remove the book from the library.
-const removeBook = document.createElement('button');
-removeBook.classList.add('removeBtn');
-removeBook.textContent = "Delete";
-
-removeBook.addEventListener('click', removeBook);
-
-
-// button on each book’s display to change its read status. 
-const toggleRead = (e) => {
-  const title = e.target.parentNode.parentNode.firstChild.innerHTML.replaceAll(
-    '"',
-    ''
-  )
-  const book = library.getBook(title)
-
-  if (auth.currentUser) {
-    toggleBookReadStatusDB(book)
-  } else {
-    book.readStatus = !book.readStatus
-    saveLocal()
-    updateBooksGrid()
-  }
 }
-
-addBookBtn.onclick = openAddBookModal
-overlay.onclick = closeAddBookModal
-addBookForm.onsubmit = addBook
-window.onkeydown = handleKeyboardInput
-
-// Local Storage
-const saveLocal = () => {
-  localStorage.setItem('library', JSON.stringify(library.books))
-};
-
-const restoreLocal = () => {
-  const books = JSON.parse(localStorage.getItem('library'))
-  if (books) {
-    library.books = books.map((book) => JSONToBook(book))
-  } else {
-    library.books = []
-  }
-}; */
